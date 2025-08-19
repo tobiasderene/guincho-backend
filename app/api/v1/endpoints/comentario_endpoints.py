ccb21ab7119a392d9453cfdfacb8632c34ba2762
KanutoMR <marcelo.rodriguez@upa.edu.py>
2025-08-19 08:51:31 -0300

    endpoint comentario correction

--- app/api/v1/endpoints/comentario_endpoints.py
+++ app/api/v1/endpoints/comentario_endpoints.py
@@ -4,23 +4,23 @@ from typing import List
 
 from app.db.database import get_db
 from app.db.models import Comentario
-from app.schemas.comentarios import CategoriaVehiculosCreate, CategoriaVehiculosOut
+from app.schemas.comentarios import ComentarioCreate, ComentarioOut
 
 router = APIRouter()
 
-@router.post("/", response_model=CategoriaVehiculosOut, status_code=status.HTTP_201_CREATED)
-def crear_comentario(comentario: CategoriaVehiculosCreate, db: Session = Depends(get_db)):
+@router.post("/", response_model=ComentarioOut, status_code=status.HTTP_201_CREATED)
+def crear_comentario(comentario: ComentarioCreate, db: Session = Depends(get_db)):
     nuevo_comentario = Comentario(**comentario.dict())
     db.add(nuevo_comentario)
     db.commit()
     db.refresh(nuevo_comentario)
     return nuevo_comentario
 
-@router.get("/", response_model=List[CategoriaVehiculosOut])
+@router.get("/", response_model=List[ComentarioOut])
 def obtener_comentarios(db: Session = Depends(get_db)):
     return db.query(Comentario).all()
 
-@router.get("/publicacion/{id_publicacion}", response_model=List[CategoriaVehiculosOut])
+@router.get("/publicacion/{id_publicacion}", response_model=List[ComentarioOut])
 def obtener_comentarios_por_publicacion(id_publicacion: int, db: Session = Depends(get_db)):
     return db.query(Comentario).filter(Comentario.id_publicacion == id_publicacion).all()
 
