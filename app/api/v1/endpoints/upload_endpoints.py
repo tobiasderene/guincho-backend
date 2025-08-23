1183aa7bb96f6d9303b0d1caae986ac83a5ad1c5
KanutoMR <marcelo.rodriguez@upa.edu.py>
2025-08-23 20:20:26 -0300

    content type

--- app/api/v1/endpoints/upload_endpoints.py
+++ app/api/v1/endpoints/upload_endpoints.py
@@ -49,6 +49,7 @@ def get_signed_url(
             version="v4",
             expiration=timedelta(minutes=10),
             method="PUT",
+            content_type="image/jpeg", 
             credentials=signing_credentials,
         )
 
