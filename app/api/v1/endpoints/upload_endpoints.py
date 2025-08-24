305d2b63ba6ff6c5d20b9ac0af44373341d3067b
KanutoMR <marcelo.rodriguez@upa.edu.py>
2025-08-24 17:47:04 -0300

    no signed url

--- app/api/v1/endpoints/upload_endpoints.py
+++ app/api/v1/endpoints/upload_endpoints.py
@@ -9,57 +9,21 @@ from app.core.security import get_current_user
 from app.db.database import get_db
 import logging
 
-logger = logging.getLogger(__name__)
-
 router = APIRouter()
+BUCKET_NAME = os.getenv("BUCKET_NAME")
 
-BUCKET_NAME = os.getenv("GCS_BUCKET_NAME")
-
-credentials, _ = default()
-
-# then within your abstraction
-auth_request = requests.Request()
-credentials.refresh(auth_request)
-
-signing_credentials = compute_engine.IDTokenCredentials(
-    auth_request,
-    "",
-    service_account_email=credentials.service_account_email
-)
-
-@router.get("/signed-url")
-def get_signed_url(
-    filename: str = Query(...),
-    current_user: dict = Depends(get_current_user),
-    db: Session = Depends(get_db),
-):
+@router.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
     try:
-        if not filename:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="El parámetro filename es obligatorio."
-            )
-
-        logger.info(f"Generando signed URL para: {filename}")
         client = storage.Client()
         bucket = client.bucket(BUCKET_NAME)
-        blob = bucket.blob(filename)
+        blob = bucket.blob(file.filename)
 
-        upload_url = blob.generate_signed_url(
-            version="v4",
-            expiration=timedelta(minutes=10),
-            method="PUT",
-            credentials=signing_credentials,
-        )
+        # sube el archivo al bucket
+        blob.upload_from_file(file.file, content_type=file.content_type)
 
-        public_url = f"https://storage.googleapis.com/{BUCKET_NAME}/{filename}"
-        logger.info(f"URL generada correctamente: {upload_url}")
+        public_url = f"https://storage.googleapis.com/{BUCKET_NAME}/{file.filename}"
 
-        return {"upload_url": upload_url, "public_url": public_url}
-
+        return {"public_url": public_url}
     except Exception as e:
-        logger.exception("Error generando signed URL")
-        raise HTTPException(
-            status_code=500,
-            detail=f"Error interno: {str(e)}"
-        )
+        raise HTTPException(status_code=500, detail=str(e))
